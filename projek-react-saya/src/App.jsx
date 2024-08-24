1019b8d304effe6c4f1862487be9461a34c6edd3
Diggi4 <digie444@gmail.com>
2024-08-24 20:51:53 +0700

    mengubah kata pada tombol count

--- projek-react-saya/src/App.jsx
+++ projek-react-saya/src/App.jsx
@@ -19,7 +19,7 @@ function App() {
       <h1>Belajar React Itu Gampang Banget</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+          hitungan ke {count}
         </button>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
